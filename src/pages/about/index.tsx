ed3ade395ae76fee9bffe0b764e863c4e9f02f8f
Luis Gaspari <luisgaspari@gmail.com>
2024-05-23 20:27:34 -0300

    added About page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Box, Stack, Heading, Center, Container, useColorModeValue } from "@chakra-ui/react";
 import Layout from "../../components/layout";
 
 function About() {
@@ -15,12 +15,48 @@ function About() {
         position="relative"
         borderRadius="3xl"
       >
-        <Text fontSize={100} color="gray.300">
-          Sobre
-        </Text>
+            <Box p={4}>
+                <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
+                    <Heading fontSize={'3xl'}>Sobre</Heading>
+                    <Text color={'gray.600'} fontSize={'xl'}>
+                        Informações sobre o projeto e o desenvolvedor.
+                    </Text>
+                    <Center py={6}>
+                        <Box
+                            maxW={'520px'}
+                            w={'full'}
+                            bg={useColorModeValue('white', 'gray.900')}
+                            boxShadow={'2xl'}
+                            rounded={'lg'}
+                            p={6}
+                            textAlign={'center'}>
+                            <Heading fontSize={'2xl'} fontFamily={'body'}>
+                                Grupo 2
+                            </Heading>
+                            <Text fontWeight={600} color={'gray.500'} mb={4}>
+                                Diego <br />
+                                Michael Douglas <br />
+                                Lucas Felipe de Jesus <br />
+                                Luis Antonio Gaspari <br />
+                                Wendel
+                            </Text>
+                            <Text
+                                textAlign={'center'}
+                                color={useColorModeValue('gray.700', 'gray.400')}
+                                px={3}>
+                                <Text as='b'>Fatec Indaiatuba</Text> <br />
+                                Análise e Desenvolvimento de Sistemas <br />
+                                4º Semestre - 2024
+                            </Text>
+                        </Box>
+                    </Center>
+                </Stack>
+            </Box>
       </Flex>
     </Layout>
   );
 }
 
 export default About;
+
+
